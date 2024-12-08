ff73cb2aaab1c55290ad75c49da520636d22f80b
moaazreda55 <moaazreda02@gmail.com>
2024-12-08 21:25:07 +0200

    Initial commit

---
+++ scrpt.py
@@ -0,0 +1,168 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn_features.transformers import DataFrameSelector
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score, confusion_matrix, accuracy_score, roc_curve, auc
+from PIL import Image
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+
+df = pd.read_csv("Customer-Churn-Records.csv")
+
+num_cols = ['Age', 'CreditScore', 'Balance', 'EstimatedSalary']
+categ_cols = ['Gender', 'Geography']
+
+
+
+df.drop(columns=['RowNumber', 'CustomerId', 'Surname','Card Type','Complain'], axis=1, inplace=True)
+
+X = df.drop(columns=['Exited'], axis=1)
+y = df['Exited']
+
+## Split to train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=45)
+
+ready_cols = list(set(X_train.columns.tolist()) - set(num_cols) - set(categ_cols))
+
+num_pipeline = Pipeline(steps=[
+                        ('selector', DataFrameSelector(num_cols)),
+                        ('imputer', SimpleImputer(strategy='median')),
+                        ('scaler', StandardScaler())
+                    ])
+
+
+categ_pipeline = Pipeline(steps=[
+                        ('selector', DataFrameSelector(categ_cols)),
+                        ('imputer', SimpleImputer(strategy='most_frequent')),
+                        ('ohe', OneHotEncoder(drop='first', sparse_output=False))
+                    ])
+
+ready_pipeline = Pipeline(steps=[
+                        ('selector', DataFrameSelector(ready_cols)),
+                        ('imputer', SimpleImputer(strategy='most_frequent'))])
+
+all_pipeline = FeatureUnion(transformer_list=[
+                                    ('numerical', num_pipeline),
+                                    ('categorical', categ_pipeline),
+                                    ('ready', ready_pipeline)])  
+
+X_train_final = all_pipeline.fit_transform(X_train)
+X_test_final = all_pipeline.transform(X_test)
+
+vals_count = 1 - (np.bincount(y_train) / len(y_train))
+vals_count = vals_count / np.sum(vals_count)  ## normalizing
+
+
+dict_weights = {}
+for i in range(2):  ## 2 classes (0, 1)
+    dict_weights[i] = vals_count[i]
+
+over = SMOTE(sampling_strategy=0.7)
+X_train_resmapled, y_train_resampled = over.fit_resample(X_train_final, y_train)
+
+def train_model(X_train, y_train, plot_name, class_weight=None):
+
+    global clf_name
+
+    # Try random forest
+    clf = RandomForestClassifier(n_estimators=343, max_depth=10, 
+                                    random_state=45, class_weight=class_weight)
+    clf.fit(X_train, y_train)
+    y_pred_train = clf.predict(X_train)
+    y_pred_test = clf.predict(X_test_final)
+    
+    ## metrics
+    score_train = f1_score(y_train, y_pred_train)
+    f1_test = f1_score(y_test, y_pred_test)
+    acc_test = accuracy_score(y_test, y_pred_test)
+
+        
+    clf_name = clf.__class__.__name__
+
+    ## Plot the confusion matrix and save it to mlflow
+    plt.figure(figsize=(10, 6))
+    sns.heatmap(confusion_matrix(y_test, y_pred_test), annot=True, cbar=False, fmt='.2f', cmap='Blues')
+    plt.title(f'{plot_name}')
+    plt.xticks(ticks=np.arange(2) + 0.5, labels=[False, True])
+    plt.yticks(ticks=np.arange(2) + 0.5, labels=[False, True])
+
+    # Save the plot to MLflow
+    plt.savefig(f'{plot_name}.png', bbox_inches='tight', dpi=300)
+    plt.close()
+
+    with open('metrics.txt', 'a') as f:
+            f.write(f'{clf_name} {plot_name}\n')
+            f.write(f"F1-score of Training is: {score_train*100:.2f} %\n")
+            f.write(f"F1-Score of Validation is: {f1_test*100:.2f} %\n")
+            f.write('----'*10 + '\n')
+
+
+    return True
+
+
+    # Compute ROC curve and AUC
+    # fpr, tpr, _ = roc_curve(y_test, y_pred_test)
+    # roc_auc = auc(fpr, tpr)
+
+    # # Plot ROC curve and save it to mlflow
+    # plt.figure()
+    # plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    # plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    # plt.xlim([0.0, 1.0])
+    # plt.ylim([0.0, 1.05])
+    # plt.xlabel('False Positive Rate')
+    # plt.ylabel('True Positive Rate')
+    # plt.title('Receiver Operating Characteristic (ROC) Curve')
+    # plt.legend(loc="lower right")
+
+    # # Save the plot to MLflow
+    # roc_fig = plt.gcf()
+    # plt.close()
+
+
+train_model(X_train=X_train_final, y_train=y_train, plot_name='without-imbalance', 
+             class_weight=None)
+
+## 2. with considering the imabalancing data using class_weights
+train_model(X_train=X_train_final, y_train=y_train, plot_name='with-class-weights', 
+             class_weight=dict_weights)
+
+## 3. with considering the imabalancing data using oversampled data (SMOTE)
+train_model(X_train=X_train_resmapled, y_train=y_train_resampled, plot_name=f'with-SMOTE', 
+             class_weight=None)
+
+
+confusion_matrix_paths = [f'./without-imbalance.png', f'./with-class-weights.png', f'./with-SMOTE.png']
+
+## Load and plot each confusion matrix
+plt.figure(figsize=(15, 5))  # Adjust figure size as needed
+for i, path in enumerate(confusion_matrix_paths, 1):
+    img = Image.open(path)
+    plt.subplot(1, len(confusion_matrix_paths), i)
+    plt.imshow(img)
+    plt.axis('off')  # Disable axis for cleaner visualization
+
+
+## Save combined plot locally
+plt.suptitle(clf_name, fontsize=16)
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+plt.savefig(f'conf_matrix.png', bbox_inches='tight', dpi=300)
+
+## Delete old image files
+for path in confusion_matrix_paths:
+    os.remove(path)
+
+
